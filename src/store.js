e6c175551839cbb1fb952c972bfabcc769b50b0c
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-02 05:21:58 +0200

    added store.js

---
+++ src/store.js
@@ -0,0 +1,24 @@
+import { create } from "zustand"
+
+const useStore = create((set) => ({
+    thumbColor: '#000000',
+    trackColor: '#FFFFFF',
+    borderColor: '#FFFFFF',
+    cornerColor: '#FFFFFF',
+    width: '10px',
+    borderRadius: '0px',
+    borderWidth: '0px',
+    borderStyle: 'solid',
+    firefoxWidth: 'auto',
+    changeThumbColor: (color) => set({ thumbColor: color }),
+    changeTrack: (color) => set({ trackColor: color }),
+    changeBorderColor: (color) => set({ borderColor: color }),
+    changeCornerColor: (color) => set({ cornerColor: color }),
+    changeWidth: (px) => set({ width: px }),
+    changeBorderRadius: (px) => set({ borderRadius: px }),
+    changeBorderWidth: (px) => set({ borderWidth: px }),
+    changeBorderStyle: (style) => set({ borderStyle: style }),
+    changeFirefoxWidth: (style) => set({ firefoxWidth: style }),
+}))
+
+export default useStore
