2f503b5dbe26291022ff3e2144b4a47470756554
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-04 23:52:40 +0200

    added Code component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Properties from './Properties'
 import Window from './Window'
+import Code from './Code'
 
 export default function Main() {
   return (
-    <main className='max-w-7xl mx-auto xl:w-full px-5 grid grid-cols-[25%_75%] m1100:grid-cols-[33%_66%] m800:grid-cols-[40%_60%] m700:grid-cols-[47%_53%] m600:grid-cols-[1fr] m600:w-4/5 m400:w-full mt-[50px]'>
+    <main className='max-w-[1400px] mx-auto xl:w-full px-5 grid gap-5 grid-cols-[300px_1fr_330px] m1100:grid-cols-[300px_1fr] m700:grid-cols-[250px_1fr] m600:grid-cols-[1fr] m600:w-4/5 m500:w-full mt-[50px]'>
         <Properties />
         <Window />
+        <Code />
     </main>
   )
 }
